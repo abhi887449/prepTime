43a7434c1d2b8884853ff284131efc420d46d2d7
Abhishek Singh <abhisheksingh9793510@gmail.com>
2024-05-28 16:59:15 +0530

    Page view added

--- components/StartRelaxStudy.js
+++ components/StartRelaxStudy.js
@@ -3,43 +3,47 @@ import { Button } from "@nextui-org/react";
 import React from "react";
 
 const StartRelaxStudy = () => {
-  const { userPrepTimeData, updateStatusStuding, updateStatusResting,setStreakIncremented,setStreakOne } =
-    usePrepTimeStore((state) => ({
-      userPrepTimeData: state.userPrepTimeData,
-      updateStatusStuding: state.updateStatusStuding,
-      updateStatusResting: state.updateStatusResting,
-      setStreakIncremented: state.setStreakIncremented,
-      setStreakOne: state.setStreakOne,
-    }));
-    function isDifferenceOneDay(startDate) {
-        // Convert dates to milliseconds
-        const startMillis = new Date(startDate).getTime();
-        const endMillis = new Date(new Date().toLocaleDateString()).getTime();
-    
-        // Calculate the absolute difference in milliseconds
-        const differenceMillis = Math.abs(endMillis - startMillis);
-    
-        // Calculate the number of milliseconds in a day
-        const millisecondsInDay = 1000 * 60 * 60 * 24;
-    
-        // Check if the difference is exactly 1 day
-        // console.log(startDate)
-        return differenceMillis === millisecondsInDay;
-    }
-    const handleStartStudy = ()=>{
-        updateStatusStuding()
-        if(userPrepTimeData.streakDays >=1){
-            let todayDate = new Date().toLocaleDateString()
-            if(!(todayDate===userPrepTimeData.lastVisitedDay)){
-                if(isDifferenceOneDay(userPrepTimeData.lastVisitedDay)){
-                    setStreakIncremented()
-                }
-                else{
-                    setStreakOne()
-                }
-            }
+  const {
+    userPrepTimeData,
+    updateStatusStuding,
+    updateStatusResting,
+    setStreakIncremented,
+    setStreakOne,
+  } = usePrepTimeStore((state) => ({
+    userPrepTimeData: state.userPrepTimeData,
+    updateStatusStuding: state.updateStatusStuding,
+    updateStatusResting: state.updateStatusResting,
+    setStreakIncremented: state.setStreakIncremented,
+    setStreakOne: state.setStreakOne,
+  }));
+  function isDifferenceOneDay(startDate) {
+    // Convert dates to milliseconds
+    const startMillis = new Date(startDate).getTime();
+    const endMillis = new Date(new Date().toLocaleDateString()).getTime();
+
+    // Calculate the absolute difference in milliseconds
+    const differenceMillis = Math.abs(endMillis - startMillis);
+
+    // Calculate the number of milliseconds in a day
+    const millisecondsInDay = 1000 * 60 * 60 * 24;
+
+    // Check if the difference is exactly 1 day
+    // console.log(startDate)
+    return differenceMillis === millisecondsInDay;
+  }
+  const handleStartStudy = () => {
+    updateStatusStuding();
+    if (userPrepTimeData.streakDays >= 1) {
+      let todayDate = new Date().toLocaleDateString();
+      if (!(todayDate === userPrepTimeData.lastVisitedDay)) {
+        if (isDifferenceOneDay(userPrepTimeData.lastVisitedDay)) {
+          setStreakIncremented();
+        } else {
+          setStreakOne();
         }
+      }
     }
+  };
   return (
     <div className="flex flex-col mt-10 gap-7 mb-3">
       <Button
@@ -60,6 +64,8 @@ const StartRelaxStudy = () => {
       >
         Take a Break
       </Button>
+        <img className="self-center mt-10" src="https://visitcount.itsvg.in/api?id=preptime&label=Page%20Views&color=2&icon=5&pretty=true" />
+
     </div>
   );
 };
--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,26 @@
 import "@/styles/globals.css";
-import {NextUIProvider} from "@nextui-org/react";
-import {ThemeProvider as NextThemesProvider} from "next-themes";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export default function App({ Component, pageProps }) {
   return (
     <NextUIProvider>
       <NextThemesProvider attribute="class" defaultTheme="dark">
-      <Component {...pageProps} />
-      <div className="flex justify-center"><footer className="relative bottom-0 left-0 w-full text-white text-center py-4">
-      <p className="text-sm">
-        &copy; {2024} : <a target="_blank" className="underline underline-offset-2" href="https://github.com/abhi887449">Abhishek Singh</a>
-      </p>
-    </footer></div>
+        <Component {...pageProps} />
+        <div className="flex justify-center">
+          <footer className="relative bottom-0 left-0 w-full text-white text-center py-4">
+            <p className="text-sm">
+              &copy; {2024} :{" "}
+              <a
+                target="_blank"
+                className="underline underline-offset-2"
+                href="https://github.com/abhi887449"
+              >
+                Abhishek Singh
+              </a>
+            </p>
+          </footer>
+        </div>
       </NextThemesProvider>
     </NextUIProvider>
   );
